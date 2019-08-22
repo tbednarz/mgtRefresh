3e24422b35cba13f20698ac5f0bad8bed0d60271
MasterTres <tresbednarz@gmail.com>
2019-08-22 17:03:29 -0400

    typos

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
 var buttons = document.querySelectorAll("button");
 for (button in buttons) {
-    buttons[button].onclick = chColor(buttons)
-}
-
-function chColor() {
-    var yellowButton = document.querySelectorAll(".yellow");
-    if (this.className == "green") {
+    buttons[button].onclick = function chColor() {
         console.log('test')
-        if (yellowButton) yellowButton.className = "green";
-        this.className = "yellow";
+        var yellowButton = document.querySelectorAll(".yellow")[0];
+        if (this.className == "green") {
+            if (yellowButton) yellowButton.className = "green";
+            this.className = "yellow";
+        }
     }
 }
+
+
