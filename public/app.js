af7875a45d4cbf27d4303322d4c1dade841296b4
MasterTres <tresbednarz@gmail.com>
2019-08-23 17:37:53 -0400

    js for toggle buttons

--- public/app.js
+++ public/app.js
@@ -1,13 +1,49 @@
 var buttons = document.querySelectorAll("button");
 for (button in buttons) {
-    buttons[button].onclick = function chColor() {
+    buttons[button].onclick = function () {
         console.log('test')
-        var yellowButton = document.querySelectorAll(".yellow")[0];
-        if (this.className == "green") {
-            if (yellowButton) yellowButton.className = "green";
-            this.className = "yellow";
-        }
+        buttons.forEach(function (btn) {
+            btn.classList.remove('highlight');
+        })
+        this.classList.add('highlight');
     }
 }
 
+// let buttons = document.getElementsByTagName('button');
+// for (button in buttons) {
+//     buttons[button].onclick = function () {
+//         console.log('test')
+//         var yellowButton = document.querySelectorAll(".yellow")[0];
+//         if (this.className == "green") {
+//             if (yellowButton) yellowButton.className = "green";
+//             this.className = "yellow";
+//         }
+//     }
+// }
 
+
+// var buttons = document.querySelectorAll(".green");
+// for (button in buttons) {
+//     buttons[button].onclick = function () {
+//         console.log('test')
+//         var yellowButton = document.querySelectorAll(".yellow")[0];
+//         if (this.className == "green") {
+//             if (yellowButton) yellowButton.className = "green";
+//             this.className = "yellow";
+//         }
+//     }
+// }
+
+//  var buttons = document.querySelectorAll("button");
+
+//  for (button in buttons) {
+//      buttons[button].onclick = function () {
+//          console.log('test')
+//          buttons.forEach(function (btn) {
+//              btn.classList.remove('highlight');
+//          })
+//          this.classList.add('highlight');
+//      }
+
+
+//  }
